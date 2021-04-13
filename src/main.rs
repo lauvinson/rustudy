cbfb4c28f87fa75ef9c98d9d8f105259128f7579
vinson <liuws@zhiyun-tech.com>
2021-04-13 14:37:04 +0800

    Be familiar with the file hierarchy and how to call function of required module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// mod print;
+// This declaration will look for a file named 'strings.rs' or 'strings/mod.rs' and
+// will insert its content inside a module named 'strings' under this scope
+mod strings;
+// single line comments
+/*
+ multi line comments
+ */
+/// <p>html comments</p>
+
 fn main() {
-    println!("Hello, world!");
+    // let x = 5 + 5;
+    // println!("Is `x` 10 or 100? x = {}", x)
+    strings::print();
 }
